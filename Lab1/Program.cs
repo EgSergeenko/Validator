f5769c1e6040bdf024f1ff61c233d54dda658810
EgSergeenko <es.egor.sergeenko@gmail.com>
2020-03-03 18:19:38 +0300

    Deleted tests

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,17 +6,7 @@ namespace ValidationSystem
     {
         static void Main()
         {
-            Validator v = new Validator("a");
-            Student s1 = new Student("Ivan");
-            Student s2 = new Student("Dan");
-            s1.DoTask("a");
-            s2.DoTask("b");
-            v.AddStudent(s1);
-            v.AddStudent(s2);
-            v.Validate(s1);
-            v.Validate(s2);
-            v.ShowInfo();
-            Console.ReadKey();
+
         }
     }
 }
